9c58358fa8f08bce47aae40210c2b887c1e37bff
bruno <bruno.lusardi.96@gmail.com>
2024-07-10 13:57:39 +0200

    create base user controllers

---
+++ controllers/user-controllers.js
@@ -0,0 +1,20 @@
+// @desc    Register new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = (req, res) => {
+  res.send("register user");
+};
+
+// @desc    Auth the user and generate token
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = (req, res) => {
+  res.send("login user");
+};
+
+// @desc    logout
+// @route   POST /api/users/logout
+// @access  Private
+const logoutUser = (req, res) => {
+  res.send("logout user");
+};
