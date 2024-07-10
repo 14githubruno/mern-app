c0c384437a39561f48844a1ec0e0f1a0f895cbd4
bruno <bruno.lusardi.96@gmail.com>
2024-07-10 13:59:55 +0200

    create user routes and export user router

---
+++ routes/user-routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { userControllers } from "../controllers/user-controllers";
+
+const router = express.Router();
+
+router.route("/").post(userControllers.registerUser);
+router.route("/login").post(userControllers.loginUser);
+router.route("/logout").post(userControllers.logoutUser);
+
+export default router;
