d52de01b924dc4942edaf0f5a8fcc168c92dbb0a
bruno <bruno.lusardi.96@gmail.com>
2024-07-10 14:03:49 +0200

    allow sending FormData and parsing json

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ import userRouter from "./routes/user-routes.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/users", userRouter);
 
 // testing
