815974f62aee21425efb5cf018f3df05bad351f0
bruno <bruno.lusardi.96@gmail.com>
2024-07-29 13:35:10 +0200

    handle user logging in and generate token

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/user-model.js";
 import bcrypt from "bcrypt";
+import { generateToken } from "../lib/generate-token.js";
 
 // @desc    Register new user
 // @route   POST /api/users/register
@@ -47,11 +48,34 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc    Auth the user and generate token
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
-  res.status(200).json({
-    message: "user logged in",
-  });
-};
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(400);
+    throw new Error("Credentials are not valid");
+  }
+
+  const passwordsMatch = await bcrypt.compare(password, user.password);
+  if (user && passwordsMatch) {
+    res.status(200).json({
+      message: `User [${user.name}] is logged in`,
+      body: {
+        _id: user._id,
+        name: user.name,
+        token: generateToken(user._id),
+      },
+    });
+  } else {
+    res.status(400);
+    throw new Error("Credentials are not valid");
+  }
+});
 
 // @desc    logout
 // @route   POST /api/users/logout
