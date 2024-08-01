14a79b45d6553c352faa43e05faffe3115598927
bruno <bruno.lusardi.96@gmail.com>
2024-08-01 15:44:55 +0200

    provide browser router in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./styles/@forward/main.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { browserRouter } from "./browser-router/browser-router";
+import { RouterProvider } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={browserRouter} />
+);
