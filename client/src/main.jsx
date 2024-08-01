eabee9ed0528ed09c5d68756b02e4938d2ec2f4f
bruno <bruno.lusardi.96@gmail.com>
2024-08-01 15:47:29 +0200

    add comment in main.jsx for browser router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/@forward/main.scss";
 
+// browser router
 import { browserRouter } from "./browser-router/browser-router";
 import { RouterProvider } from "react-router-dom";
 
