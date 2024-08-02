173902e4065f5d4a7999e5b0eacb54437065cadb
bruno <bruno.lusardi.96@gmail.com>
2024-08-02 09:56:55 +0200

    use redux-toolkit docs example for testing

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/auth-slice";
+import counterReducer from "./features/counter/counter-slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    counter: counterReducer,
   },
 });
 
