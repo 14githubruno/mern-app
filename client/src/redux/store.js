6519b31a76895e846ed81f005caf68ff87a1e256
bruno <bruno.lusardi.96@gmail.com>
2024-08-02 13:55:55 +0200

    add api slice to redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/api-slice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import authReducer from "./features/auth/auth-slice";
 
 const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
+setupListeners(store.dispatch);
 export { store };
