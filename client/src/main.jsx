2b42f41e04b0329656c757e14e4dcb0cc0c06529
bruno <bruno.lusardi.96@gmail.com>
2024-08-02 09:44:52 +0200

    provide store to entire app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/@forward/main.scss";
 
+// redux
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
 // browser router
 import { browserRouter } from "./browser-router/browser-router";
 import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={browserRouter} />
+  <Provider store={store}>
+    <RouterProvider router={browserRouter} />
+  </Provider>
 );
