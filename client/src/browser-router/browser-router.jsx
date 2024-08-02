e56b2a2ad36d9199075fd628e74ff1a2656523b4
bruno <bruno.lusardi.96@gmail.com>
2024-08-02 14:00:17 +0200

    refact browser-router to include new pages and PrivateRoute component

--- client/src/browser-router/browser-router.jsx
+++ client/src/browser-router/browser-router.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import PrivateRoute from "../components/private-route";
 import Homepage from "../pages/homepage";
+import Register from "../pages/register";
+import Login from "../pages/login";
+import Dashboard from "../pages/dashboard";
 
 const browserRouter = createBrowserRouter([
   {
@@ -10,6 +14,23 @@ const browserRouter = createBrowserRouter([
       {
         path: "/",
         element: <Homepage />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        element: <PrivateRoute />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+          },
+        ],
       },
     ],
   },
