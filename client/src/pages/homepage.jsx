7a70730b232c8549bf918ce149b56590e717c948
bruno <bruno.lusardi.96@gmail.com>
2024-08-02 10:30:46 +0200

    use docs to get rtk-query going

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -2,11 +2,15 @@ import HomeHeadingOne from "../components/ui/home-heading-one/home-heading-one";
 import HomeTelevision from "../components/ui/home-television/home-television";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "../redux/features/counter/counter-slice";
+import { useGetPokemonByNameQuery } from "../redux/api/pokemon";
 
 export default function Homepage() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
-
+  // Using a query hook automatically fetches data and returns query values
+  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+  // Individual hooks are also accessible under the generated endpoints:
+  // const { data, error, isLoading } = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
   return (
     <section>
       <HomeHeadingOne />
@@ -27,6 +31,16 @@ export default function Homepage() {
             Decrement
           </button>
         </div>
+        {error ? (
+          <>Oh no, there was an error</>
+        ) : isLoading ? (
+          <>Loading...</>
+        ) : data ? (
+          <>
+            <h3>{data.species.name}</h3>
+            <img src={data.sprites.front_shiny} alt={data.species.name} />
+          </>
+        ) : null}
       </div>
     </section>
   );
