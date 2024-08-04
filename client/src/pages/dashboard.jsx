1afd7e24a935de67624d81e4ac52c9d414d6fbd4
bruno <bruno.lusardi.96@gmail.com>
2024-08-04 12:49:59 +0200

    test get tvseries route and handle server response

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,11 +1,19 @@
 import { useSelector } from "react-redux";
+import { useGetAllTvseriesQuery } from "../redux/api/tvseries-api-slice";
 
 export default function Dashboard() {
   const user = useSelector((state) => state.auth.user);
+  const { data, isSuccess } = useGetAllTvseriesQuery();
+
+  let paragraph;
+  if (isSuccess) {
+    paragraph = data.body ? JSON.stringify(data) : data.message;
+  }
 
   return (
     <section>
       <h1 style={{ color: "whitesmoke" }}>{user}</h1>
+      <p style={{ color: "whitesmoke" }}>{paragraph}</p>
     </section>
   );
 }
