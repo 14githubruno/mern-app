9df3d878a56ae77e48abd5f7717eb529ca0991ef
bruno <bruno.lusardi.96@gmail.com>
2024-08-04 12:11:16 +0200

    add tvseries api constant

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,5 +1,6 @@
 const DEV_BASE_URL = "http://localhost:3000";
 const PROD_BASE_URL = "http://localhost:3000"; // will be replaced
 const API_USERS = "/api/users";
+const API_TVSERIES = "/api/tvseries";
 
-export { DEV_BASE_URL, PROD_BASE_URL, API_USERS };
+export { DEV_BASE_URL, PROD_BASE_URL, API_USERS, API_TVSERIES };
