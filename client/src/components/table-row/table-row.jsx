042afcd87cdd24a6fdccee5bf770f070cb34ce7f
bruno <bruno.lusardi.96@gmail.com>
2024-08-05 15:29:23 +0200

    create table-row component

---
+++ client/src/components/table-row/table-row.jsx
@@ -0,0 +1,44 @@
+import styles from "./table-row.module.scss";
+import { AiFillStar } from "react-icons/ai";
+import { RxPencil1, RxMagnifyingGlass, RxTrash } from "react-icons/rx";
+import { Link } from "react-router-dom";
+
+export default function TableRow({ num, id, title, stars, image, note }) {
+  const arrayOfStars = new Array(Number(stars)).fill("*");
+  const restOfStars = new Array(5 - arrayOfStars.length).fill("*");
+
+  return (
+    <div className={styles.tableRow} key={id}>
+      <div className={styles.num}>{num}</div>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.starsWrapper}>
+        {arrayOfStars.map((star, index) => {
+          return <AiFillStar key={index} className={styles.cardStarGood} />;
+        })}
+        {restOfStars === 0
+          ? null
+          : restOfStars.map((star, index) => {
+              return <AiFillStar key={index} className={styles.cardStar} />;
+            })}
+      </div>
+      <div
+        className={styles.image}
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
+      ></div>
+      <div className={styles.note}>{note}</div>
+      <div className={styles.iconsContainer}>
+        <span className={styles.iconView}>
+          <RxMagnifyingGlass className={styles.icon} />
+        </span>
+        <Link className={styles.iconEdit}>
+          <RxPencil1 className={styles.icon} />
+        </Link>
+        <span className={styles.iconTrash}>
+          <RxTrash className={styles.icon} />
+        </span>
+      </div>
+    </div>
+  );
+}
