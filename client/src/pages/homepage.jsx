1d4e4c5652534c18a4030cfd3760ee9c7600a18b
bruno <bruno.lusardi.96@gmail.com>
2024-08-05 16:58:54 +0200

    (refact file system structure of components: for now, remove ui folder and store all components in components folder)

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,6 +1,6 @@
-import HomeHeadingOne from "../components/ui/home-heading-one/home-heading-one";
-import HomeParagraph from "../components/ui/home-paragraph/home-paragraph";
-import HomeTelevision from "../components/ui/home-television/home-television";
+import HomeHeadingOne from "../components/home-heading-one/home-heading-one";
+import HomeParagraph from "../components/home-paragraph/home-paragraph";
+import HomeTelevision from "../components/home-television/home-television";
 
 export default function Homepage() {
   return (
