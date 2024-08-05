e5e81a7692fb789e07b52affc1a2d3ed5e26044c
bruno <bruno.lusardi.96@gmail.com>
2024-08-05 15:30:24 +0200

    refact dashboard page to use table -- add some style in _global.scss partial

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,23 +1,43 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { useGetAllTvseriesQuery } from "../redux/api/tvseries-api-slice";
+import { setTvseries } from "../redux/features/tvseries/tvseries-slice";
+import Table from "../components/table/table";
 
 export default function Dashboard() {
+  const dispatch = useDispatch();
   const user = useSelector((state) => state.auth.user);
+  const tvseries = useSelector((state) => state.tvseries.tvseries);
   const { data, isSuccess } = useGetAllTvseriesQuery();
 
-  let paragraph;
-  if (isSuccess) {
-    paragraph = data.body ? JSON.stringify(data) : data.message;
-  }
+  // sort and set rows to be displayed in the table
+  useEffect(() => {
+    if (isSuccess && data.body) {
+      const sortedItems = data?.body
+        .slice()
+        .sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+      dispatch(setTvseries([...sortedItems]));
+    }
+  }, [data, isSuccess]);
 
   return (
     <section>
-      <h1 style={{ color: "whitesmoke" }}>{user}</h1>
-      <p style={{ color: "whitesmoke" }}>{paragraph}</p>
-      <Link style={{ color: "whitesmoke" }} to={"/dashboard/kreate-tvseries"}>
-        kreate a tvseries
-      </Link>
+      <p className="dashboardParag">
+        This is your dashboard,{" "}
+        <span className="dashboardUsername">{user}</span>
+        <span className="dashboardCTAParag">
+          If you want to kreate a table row,{" "}
+          <Link
+            className="dashboardKreateLink"
+            to={"/dashboard/kreate-tvseries"}
+          >
+            klick here
+          </Link>
+        </span>
+      </p>
+      {/* display table with rows */}
+      <Table tvseries={tvseries} />
     </section>
   );
 }
