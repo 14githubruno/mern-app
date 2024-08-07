8e1559187b015bb2a1af9b3c7a0c58c04fdc9fe0
bruno <bruno.lusardi.96@gmail.com>
2024-08-07 10:25:17 +0200

    refact loader props overridden and color

--- client/src/components/loader/loader.jsx
+++ client/src/components/loader/loader.jsx
@@ -4,13 +4,13 @@ export default function Loader() {
   const propsToOverride = {
     display: "block",
     margin: "5rem auto 0 auto",
-    borderColor: "var(--aqua-green-color)",
   };
 
   return (
     <ClipLoader
       cssOverride={propsToOverride}
       size={100}
+      color="var(--aqua-green-color)"
       aria-label="Loading Spinner"
     />
   );
