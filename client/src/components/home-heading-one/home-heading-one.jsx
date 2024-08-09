b339dd0e3342ab39922563ab22f9b5c337b0ab0f
bruno <bruno.lusardi.96@gmail.com>
2024-08-09 14:28:08 +0200

    refact/rebuilt homeHeadingOne component of home page

--- client/src/components/home-heading-one/home-heading-one.jsx
+++ client/src/components/home-heading-one/home-heading-one.jsx
@@ -3,18 +3,36 @@ import { GiNoodles } from "react-icons/gi";
 import { RiKnifeBloodLine } from "react-icons/ri";
 
 export default function HomeHeadingOne() {
+  const headingOne = "Reakt Applikation";
+
   return (
-    <h1 className={styles.homeHeadingOne}>
-      Rea<span className={styles.kLetter}>k</span>t Appli
-      <span className={styles.kLetter}>k</span>ation
-      <span>
-        <GiNoodles
-          className={`${styles.homeHeadingOneIcon} ${styles.noodlesIcon}`}
-        />
-        <RiKnifeBloodLine
-          className={`${styles.homeHeadingOneIcon} ${styles.knifeBloodIcon}`}
-        />
-      </span>
-    </h1>
+    <header>
+      <h1 className={styles.homeHeadingOne}>
+        {headingOne.split("").map((letter, index) => {
+          const space = " ";
+          const k = "k";
+          if (letter === space) return " ";
+          else
+            return (
+              <span
+                key={index}
+                className={`${styles.letter} ${
+                  letter === k ? styles.kLetter : ""
+                }`}
+              >
+                {letter}
+              </span>
+            );
+        })}
+        <span>
+          <GiNoodles
+            className={`${styles.homeHeadingOneIcon} ${styles.noodlesIcon}`}
+          />
+          <RiKnifeBloodLine
+            className={`${styles.homeHeadingOneIcon} ${styles.knifeBloodIcon}`}
+          />
+        </span>
+      </h1>
+    </header>
   );
 }
