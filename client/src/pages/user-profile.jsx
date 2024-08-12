5869d1b0bc8b2cb1582610897744a8e21945a44f
bruno <bruno.lusardi.96@gmail.com>
2024-08-12 17:54:05 +0200

    create base logic to delete user using users api slice

--- client/src/pages/user-profile.jsx
+++ client/src/pages/user-profile.jsx
@@ -1,8 +1,36 @@
-import { useGetUserProfileQuery } from "../redux/api/users-api-slice";
+import {
+  useGetUserProfileQuery,
+  useDeleteUserProfileMutation,
+} from "../redux/api/users-api-slice";
+import { useDispatch } from "react-redux";
+import { clearCredentials } from "../redux/features/auth/auth-slice";
+import { resetTvseries } from "../redux/features/tvseries/tvseries-slice";
+import { apiSlice } from "../redux/api/api-slice";
+import { useCallback } from "react";
+import toast from "react-hot-toast";
 import Loader from "../components/loader/loader";
 
 export default function UserProfile() {
+  const dispatch = useDispatch();
   const { data, isLoading, isSuccess } = useGetUserProfileQuery();
+  const [deleteUserProfile] = useDeleteUserProfileMutation();
+
+  const handleDeleteUserProfile = useCallback(
+    async (user) => {
+      try {
+        const res = await deleteUserProfile(user).unwrap();
+        if (res.message) {
+          toast.success(res.message);
+          dispatch(clearCredentials());
+          dispatch(resetTvseries());
+          dispatch(apiSlice.util.resetApiState());
+        }
+      } catch (err) {
+        toast.error(err.message);
+      }
+    },
+    [user]
+  );
 
   let content;
 
@@ -15,6 +43,9 @@ export default function UserProfile() {
       <section style={{ color: "whitesmoke" }}>
         <p>{data.body.name}</p>
         <p>{data.body.email}</p>
+        <button onClick={() => handleDeleteUserProfile(data?.body)}>
+          Delete user
+        </button>
       </section>
     );
   }
