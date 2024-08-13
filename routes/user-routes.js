b06454053e114f68c899fb05a9da408d845c08bc
bruno <bruno.lusardi.96@gmail.com>
2024-08-13 11:00:11 +0200

    add patch update user to users' routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -8,6 +8,9 @@ router.route("/register").post(userControllers.registerUser);
 router.route("/login").post(userControllers.loginUser);
 router.route("/logout").post(protectRoute, userControllers.logoutUser);
 router.route("/profile").get(protectRoute, userControllers.getUserProfile);
-router.route("/:id").delete(protectRoute, userControllers.deleteUserProfile);
+router
+  .route("/:id")
+  .patch(protectRoute, userControllers.updateUserProfile)
+  .delete(protectRoute, userControllers.deleteUserProfile);
 
 export default router;
