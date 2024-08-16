0d4a98a96fd7da4e30a50906d48a277f5b697ae7
bruno <bruno.lusardi.96@gmail.com>
2024-08-16 13:36:38 +0200

    remove commented out prepareheaders from api slice

--- client/src/redux/api/api-slice.js
+++ client/src/redux/api/api-slice.js
@@ -5,13 +5,6 @@ const baseQuery = fetchBaseQuery({
   baseUrl:
     import.meta.env.MODE === "development" ? DEV_BASE_URL : PROD_BASE_URL,
   credentials: "include",
-  // prepareHeaders: (headers, { getState }) => {
-  //   const { token } = getState().auth;
-  //   if (token) {
-  //     headers.set("authorization", `Bearer ${token}`);
-  //   }
-  //   return headers;
-  // },
 });
 
 const apiSlice = createApi({
