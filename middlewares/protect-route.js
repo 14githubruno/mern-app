d8ff2c9d14be5c428dd59bc57179514ed1a3203b
bruno <bruno.lusardi.96@gmail.com>
2024-08-16 13:50:00 +0200

    remove protectroute middleware which checks bearer token

--- middlewares/protect-route.js
+++ middlewares/protect-route.js
@@ -2,31 +2,6 @@ import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import User from "../models/user-model.js";
 
-// const protectRoute = asyncHandler(async (req, res, next) => {
-//   let token;
-//   let decoded;
-//   let thereIsToken =
-//     req.headers.authorization && req.headers.authorization.startsWith("Bearer");
-
-//   if (thereIsToken) {
-//     try {
-//       token = req.headers.authorization.split(" ")[1];
-//       decoded = jwt.verify(token, process.env.JWT_SECRET);
-//       req.user = await User.findById(decoded._id).select("-password");
-//       next();
-//     } catch (error) {
-//       console.log(error);
-//       res.status(401);
-//       throw new Error("User not authorized");
-//     }
-//   }
-
-//   if (!thereIsToken) {
-//     res.status(401);
-//     throw new Error("No token, user is not authorized");
-//   }
-// });
-
 const protectRoute = asyncHandler(async (req, res, next) => {
   if (!req.cookies || !req.cookies.jwt) {
     res.status(401);
