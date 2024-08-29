3b02f5a889478535f92741d5866d3cd3cc8b28cc
bruno <bruno.lusardi.96@gmail.com>
2024-08-29 20:22:09 +0200

    add tooltip password in input password component

--- client/src/components/input-password/input-password.jsx
+++ client/src/components/input-password/input-password.jsx
@@ -1,5 +1,6 @@
 import styles from "./input-password.module.scss";
 import { RxEyeOpen, RxEyeNone } from "react-icons/rx";
+import { IoHelp } from "react-icons/io5";
 import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 
@@ -15,14 +16,24 @@ export default function InputPassword({ placeholder }) {
   return (
     <>
       <label htmlFor="password">
-        Password<span className="label-asterisk">*</span>
+        Password
+        <span className="label-asterisk">*</span>
+        <span className={styles.passwordTooltipWrapper}>
+          <IoHelp />
+          <span className={styles.passwordTooltip}>
+            Password must be between 10 and 15 characters long and contain at
+            least one uppercase letter, one number and one special character
+            among: !#$%&?"
+          </span>
+        </span>
       </label>
       <input
         type={inputType}
         id="password"
         placeholder={placeholder}
         autoComplete="off"
-        maxLength={10}
+        minLength={10}
+        maxLength={15}
         {...register("password")}
       />
       {inputValue?.length > 0 && (
