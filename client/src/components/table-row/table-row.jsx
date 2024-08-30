b510c5f64b14aacf420a2384547a7a75cc653280
bruno <bruno.lusardi.96@gmail.com>
2024-08-30 16:03:42 +0200

    add aria label to div using img as background

--- client/src/components/table-row/table-row.jsx
+++ client/src/components/table-row/table-row.jsx
@@ -45,6 +45,7 @@ function TableRow({
             })}
       </div>
       <div
+        aria-label={`image of ${title} tv series`}
         className={styles.image}
         style={{
           backgroundImage: `url(${image})`,
