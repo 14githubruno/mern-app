3ca4cc65f9769fad448797ec22eefbe5e64c3908
bruno <bruno.lusardi.96@gmail.com>
2024-09-02 14:27:17 +0200

    remove comment from home television component - add role figure to the latter

--- client/src/components/home-television/home-television.jsx
+++ client/src/components/home-television/home-television.jsx
@@ -2,12 +2,9 @@ import styles from "./home-television.module.scss";
 import "./home-television.scss";
 import Carousel from "../carousel/carousel";
 
-// This icon below has been rebuilt and customized to be used with the carousel
-// import { PiTelevisionThin } from "react-icons/pi";
-
 export default function HomeTelevision({ carouselImages }) {
   return (
-    <div className={styles.homeTelevision}>
+    <div role="figure" className={styles.homeTelevision}>
       <div className={styles.aerials}>
         <div className={`${styles.aerial} ${styles.aerialLeft}`}></div>
         <div className={`${styles.aerial} ${styles.aerialRight}`}></div>
