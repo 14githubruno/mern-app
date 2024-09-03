78f787abcb0ca24ab002475c5577dde0e261818e
bruno <bruno.lusardi.96@gmail.com>
2024-09-03 15:12:24 +0200

    add token verification model

---
+++ models/token-verification-model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const tokenVerificationSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600,
+  },
+});
+
+export default mongoose.model("Token", tokenVerificationSchema);
