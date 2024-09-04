e6dbf74ab2db77b9a0e1a41847a95a0f8ff59398
bruno <bruno.lusardi.96@gmail.com>
2024-09-04 12:28:10 +0200

    add post verifyUser controller to user routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -5,7 +5,10 @@ import { protectRoute } from "../middlewares/protect-route.js";
 const router = express.Router();
 
 router.route("/register").post(userControllers.registerUser);
-router.route("/verify/:token").get(userControllers.verifyUser);
+router
+  .route("/verify/:token")
+  .get(userControllers.verifyToken)
+  .post(userControllers.verifyUser);
 router.route("/login").post(userControllers.loginUser);
 router.route("/logout").post(protectRoute, userControllers.logoutUser);
 router.route("/profile").get(protectRoute, userControllers.getUserProfile);
