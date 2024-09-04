da5e0363e959bd1051c8a23e7e17e5e61556ed50
bruno <bruno.lusardi.96@gmail.com>
2024-09-04 12:32:40 +0200

    refact verify user page-route accordingly to controllers refactoring and new symbol model

--- client/src/pages/verify-user.jsx
+++ client/src/pages/verify-user.jsx
@@ -1,27 +1,65 @@
 import { useParams, useNavigate } from "react-router-dom";
-import { useVerifyUserQuery } from "../redux/api/users-api-slice";
+import { useForm, FormProvider } from "react-hook-form";
 import { useEffect } from "react";
+import {
+  useVerifyTokenQuery,
+  useVerifyUserMutation,
+} from "../redux/api/users-api-slice";
+import { parseFormData, checkParsingError } from "../lib/parse-form-data";
+import Form from "../components/form/form";
 import toast from "react-hot-toast";
-import Loader from "../components/loader/loader";
 
 export default function VerifyUser() {
   const navigate = useNavigate();
   const params = useParams();
-  const { data, isSuccess, error } = useVerifyUserQuery(params.token);
+  const { error } = useVerifyTokenQuery(params.token);
+  const [verifyUser, { isLoading, isSuccess }] = useVerifyUserMutation();
+
+  const methods = useForm({
+    defaultValues: {
+      secret: "",
+    },
+  });
 
   useEffect(() => {
     if (error) {
       navigate("/", { replace: true });
       toast.error(error.data.message);
-    } else if (data && isSuccess) {
+    } else if (isSuccess) {
       navigate("/login", { replace: true });
-      toast.success(data.message);
     }
-  }, [data, error, isSuccess, navigate]);
+  }, [error, isSuccess, navigate]);
+
+  const handleUserVerification = async (data) => {
+    const parsedData = parseFormData(data);
+    const error = checkParsingError(parsedData);
+    if (error) {
+      toast.error(error);
+      return;
+    }
+
+    try {
+      const symbol = { token: params.token, secret: parsedData.secret };
+      const res = await verifyUser(symbol).unwrap();
+      toast.success(res?.message);
+    } catch (err) {
+      toast.error(err?.data?.message);
+    }
+  };
 
   return (
     <section>
-      <Loader />
+      <FormProvider {...methods}>
+        <Form
+          typeOfForm={"verify user"}
+          onSubmit={handleUserVerification}
+          formButtonProps={{
+            isLoading,
+            textOnLoading: "Verifying...",
+            text: "Verify",
+          }}
+        />
+      </FormProvider>
     </section>
   );
 }
