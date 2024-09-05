6d12f1cbe86d12e6de5738b5716b25a279bc11a3
bruno <bruno.lusardi.96@gmail.com>
2024-09-05 16:12:54 +0200

    use native browser local storage

--- client/src/redux/features/auth/auth-slice.js
+++ client/src/redux/features/auth/auth-slice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: null,
-  tokenExpirationDate: null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  tokenExpirationDate: JSON.parse(localStorage.getItem("exp")) || null,
 };
 
 export const authSlice = createSlice({
@@ -12,13 +12,19 @@ export const authSlice = createSlice({
     setCredentials: (state, action) => {
       state.user = action.payload.user;
       state.tokenExpirationDate = action.payload.tokenExpirationDate;
+      localStorage.setItem("user", JSON.stringify(state.user));
+      localStorage.setItem("exp", JSON.stringify(state.tokenExpirationDate));
     },
+
     setOnlyCredentialsUser: (state, action) => {
       state.user = action.payload.user;
+      localStorage.setItem("user", JSON.stringify(state.user));
     },
+
     clearCredentials: (state, action) => {
       state.user = null;
       state.tokenExpirationDate = null;
+      localStorage.clear();
     },
   },
 });
