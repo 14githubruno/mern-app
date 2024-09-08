00e2a36ee73f2e6031699a48b0da087af1b3593e
bruno <bruno.lusardi.96@gmail.com>
2024-09-08 11:43:40 +0200

    refact error page

--- client/src/pages/error-page.jsx
+++ client/src/pages/error-page.jsx
@@ -1,35 +1,28 @@
 import { useHeadTags } from "../hooks/use-head-tags";
-import { Link } from "react-router-dom";
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
 
 export default function ErrorPage() {
   // this below fires a useEffect
   useHeadTags("error");
 
   return (
-    <section style={{ color: "var(--white-color)" }}>
-      <header>
-        <h1
-          style={{
-            padding: "0 1rem",
-            fontSize: "2.5rem",
-            textAlign: "center",
-          }}
-        >
-          This resource doesn't exist or kan't be found
-        </h1>
-      </header>
-      <Link
-        style={{
-          padding: "1rem 2rem",
-          margin: "3rem 0 0 0",
-          fontSize: "2rem",
-          background: "var(--pink-blue-gradient)",
-        }}
-        to={"/"}
-        replace
-      >
-        Go back home
-      </Link>
-    </section>
+    <main>
+      <Header />
+      <section style={{ color: "var(--white-color)" }}>
+        <header>
+          <h1
+            style={{
+              padding: "0 4rem",
+              fontSize: "2.5rem",
+              textAlign: "center",
+            }}
+          >
+            This resource doesn't exist or kan't be found
+          </h1>
+        </header>
+      </section>
+      <Footer />
+    </main>
   );
 }
