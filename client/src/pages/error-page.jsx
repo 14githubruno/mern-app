b6f3299c28dd4dd219b2e692ffb9160bab7245e9
bruno <bruno.lusardi.96@gmail.com>
2024-09-10 18:38:17 +0200

    create error page content component and use it in error page

--- client/src/pages/error-page.jsx
+++ client/src/pages/error-page.jsx
@@ -1,28 +1,9 @@
 import { useHeadTags } from "../hooks/use-head-tags";
-import Header from "../components/header/header";
-import Footer from "../components/footer/footer";
+import ErrorPageContent from "../components/error-page-content/error-page-content";
 
 export default function ErrorPage() {
   // this below fires a useEffect
   useHeadTags("errorPage");
 
-  return (
-    <main>
-      <Header replace={true} />
-      <section style={{ color: "var(--white-color)" }}>
-        <header>
-          <h1
-            style={{
-              padding: "0 4rem",
-              fontSize: "2.5rem",
-              textAlign: "center",
-            }}
-          >
-            This resource doesn't exist or kan't be found
-          </h1>
-        </header>
-      </section>
-      <Footer />
-    </main>
-  );
+  return <ErrorPageContent />;
 }
