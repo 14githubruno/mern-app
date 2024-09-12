f9ebae5911682f35c238d844f7238b91b12232d4
bruno <bruno.lusardi.96@gmail.com>
2024-09-12 15:22:07 +0200

    return different limit in limiter middleware when url has to do with password

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,9 +1,26 @@
 import { rateLimit } from "express-rate-limit";
 import { throwError } from "../lib/throw-error.js";
 
+const routesWithPassword = {
+  register: "/register",
+  login: "/login",
+  forgotPassword: "/forgot-password",
+  resetPassword: "/reset-password",
+  updateProfile: "/profile",
+};
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 70,
+  limit: (req, res) => {
+    const { url } = req;
+
+    const isRouteHavingToDoWithPassword = Object.values(
+      routesWithPassword
+    ).some((route) => url.startsWith(route));
+
+    if (isRouteHavingToDoWithPassword) return 5;
+    else return 50;
+  },
   standardHeaders: "draft-6",
   legacyHeaders: false,
   handler: (req, res) => {
