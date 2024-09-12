061381fd3cf610d31a94b997770052e91502b0f7
bruno <bruno.lusardi.96@gmail.com>
2024-09-12 14:47:40 +0200

    use limiter in all routes except get ones -- still to check set trust proxy

--- routes/tvseries-routes.js
+++ routes/tvseries-routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { tvSeriesCtrl } from "../controllers/tvseries-controllers.js";
 import { protect } from "../middlewares/protect.js";
+import { limiter } from "../middlewares/limiter.js";
 
 const router = express.Router();
 
@@ -8,11 +9,11 @@ const router = express.Router();
 router
   .route("/")
   .get(protect, tvSeriesCtrl.getAllTvSeries)
-  .post(protect, tvSeriesCtrl.createOneTvSeries);
+  .post(protect, limiter, tvSeriesCtrl.createOneTvSeries);
 router
   .route("/:id")
-  .patch(protect, tvSeriesCtrl.updateOneTvSeries)
-  .delete(protect, tvSeriesCtrl.deleteOneTvSeries);
+  .patch(protect, limiter, tvSeriesCtrl.updateOneTvSeries)
+  .delete(protect, limiter, tvSeriesCtrl.deleteOneTvSeries);
 router
   .route("/:id/:title")
   .get(protect, tvSeriesCtrl.getOneTvseries)
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,46 +1,47 @@
 import express from "express";
 import { userCtrl } from "../controllers/user-controllers.js";
 import { protect } from "../middlewares/protect.js";
+import { limiter } from "../middlewares/limiter.js";
 
 const router = express.Router();
 
 /* PUBLIC */
 router
   .route("/register")
-  .post(userCtrl.registerUser);
+  .post(limiter, userCtrl.registerUser);
 router
   .route("/verify/:token")
   .get(userCtrl.verifyToken)
-  .patch(userCtrl.verifyUser);
+  .patch(limiter, userCtrl.verifyUser);
 router
   .route("/login")
-  .post(userCtrl.loginUser);
+  .post(limiter, userCtrl.loginUser);
 router
   .route("/forgot-password")
-  .post(userCtrl.forgotPassword);
+  .post(limiter, userCtrl.forgotPassword);
 router
   .route("/verify-password-secret/:token")
   .get(userCtrl.verifyToken)
-  .patch(userCtrl.verifyPasswordSecret);
+  .patch(limiter, userCtrl.verifyPasswordSecret);
 router
   .route("/reset-password/:token")
   .get(userCtrl.verifyToken)
-  .patch(userCtrl.resetPassword);
+  .patch(limiter, userCtrl.resetPassword);
 
 /* PRIVATE */
 router
   .route("/logout")
-  .post(protect, userCtrl.logoutUser);
+  .post(protect, limiter, userCtrl.logoutUser);
 router
   .route("/profile")
   .get(protect, userCtrl.getUserProfile);
 router
   .route("/profile/:id")
-  .patch(protect, userCtrl.updateUserProfile)
-  .delete(protect, userCtrl.deleteUserProfile);
+  .patch(protect, limiter, userCtrl.updateUserProfile)
+  .delete(protect, limiter, userCtrl.deleteUserProfile);
 router
   .route("/profile/verify/:token")
   .get(userCtrl.verifyToken)
-  .patch(protect, userCtrl.verifyUpdateUserProfile);
+  .patch(protect, limiter, userCtrl.verifyUpdateUserProfile);
 
 export default router;
