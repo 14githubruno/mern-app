4f1c71761b59cca3b52625083069a48b72c586fb
bruno <bruno.lusardi.96@gmail.com>
2024-09-13 15:42:49 +0200

    try set trust proxy to false

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const IS_DEV_MODE = NODE_ENV === "development";
 const __dirname = import.meta.dirname;
 
 const app = express();
-app.set("trust proxy", 1);
+app.set("trust proxy", false);
 
 app.use(
   cors({
