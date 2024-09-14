cfab8839575c161bcd41b3efab085150494ca42e
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 17:03:28 +0200

    add jsdoc to home paragraph component

--- client/src/components/home-paragraph/home-paragraph.jsx
+++ client/src/components/home-paragraph/home-paragraph.jsx
@@ -1,6 +1,12 @@
 import styles from "./home-paragraph.module.scss";
 import { BsFillSuitHeartFill } from "react-icons/bs";
 
+/**
+ * HomeParagraph component.
+ * It renders the paragraph of the homepage.
+ *
+ * @returns {JSX.Element} The rendered HomeParagraph component.
+ */
 export default function HomeParagraph() {
   return (
     <p className={styles.homeParagraph}>
