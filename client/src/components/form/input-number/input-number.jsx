8bd38a9a87f897dfd348517d3723c61edcaebe83
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 16:24:46 +0200

    add jsdoc to input number component

--- client/src/components/form/input-number/input-number.jsx
+++ client/src/components/form/input-number/input-number.jsx
@@ -2,6 +2,18 @@ import styles from "./input-number.module.scss";
 import { useFormContext } from "react-hook-form";
 import Label from "../label/label";
 
+/**
+ * InputNumber component.
+ * It renders an input field for numbers.
+ *
+ * (It renders the Label component too)
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {string} props.name - The name used as name and id of the input. Used also as label (this prop is passed to Label component too).
+ * @param {string} props.placeholder - The placeholder text for the input field.
+ *
+ * @returns {JSX.Element} The rendered InputNumber component.
+ */
 export default function InputNumber({ name, placeholder }) {
   const { register } = useFormContext();
 
