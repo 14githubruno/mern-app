c8c6a34be1f66c1337ef89905323d9eb1003ed97
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 16:33:53 +0200

    add jsdoc to input password component

--- client/src/components/form/input-password/input-password.jsx
+++ client/src/components/form/input-password/input-password.jsx
@@ -4,6 +4,20 @@ import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 import Label from "../label/label";
 
+/**
+ * InputPassword component.
+ * It renders an input field for the password.
+ *
+ * (The type of the input is toggled between "password" and "text" to toggle password visibility)
+ *
+ * (It renders the Label component too)
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {string} props.name - The name used as name, default type and id of the input. Used also as label (this prop is passed to Label component too).
+ * @param {string} [props.placeholder="Enter password"] - The placeholder text for the input field. Defaults to "Enter Password"
+ *
+ * @returns {JSX.Element} The rendered InputPassword component.
+ */
 export default function InputPassword({
   name,
   placeholder = "Enter password",
