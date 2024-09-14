977b0c83cf1e6e7bde3060955c360891ccdaa370
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 17:18:39 +0200

    add jsdoc to loader component

--- client/src/components/loader/loader.jsx
+++ client/src/components/loader/loader.jsx
@@ -1,5 +1,11 @@
 import styles from "./loader.module.scss";
 
+/**
+ * Loader component.
+ * It renders a loader element.
+ *
+ * @returns {JSX.Element} The rendered Loader component.
+ */
 export default function Loader() {
   return (
     <article className={styles.loader}>
