8b45dd9ba9520040384ee66042f9e557b68922a0
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 16:35:28 +0200

    add jsdoc to input text component

--- client/src/components/form/input-text/input-text.jsx
+++ client/src/components/form/input-text/input-text.jsx
@@ -1,6 +1,17 @@
 import { useFormContext } from "react-hook-form";
 import Label from "../label/label";
 
+/**
+ * InputText component.
+ * It renders an input field for text.
+ *
+ * (It renders the Label component too)
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {string} props.name - The name used as name and id of the input. Used also in its placeholder. Used also as label (this prop is passed to Label component too).
+ *
+ * @returns {JSX.Element} The rendered InputText component.
+ */
 export default function InputText({ name }) {
   const { register } = useFormContext();
 
