09d9dddb1e861704af24a3f7aa111626e23647d4
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 18:56:42 +0200

    refact jsdoc of form button component

--- client/src/components/form/form-button/form-button.jsx
+++ client/src/components/form/form-button/form-button.jsx
@@ -3,7 +3,9 @@ import styles from "./form-button.module.scss";
 /**
  * FormButton component.
  * It renders the button of the form.
- * The button has different text content depending on whether a fetching request is being made. If a fetching request is in progress, the button is temporarily disabled.
+ *
+ * (The button has different text content depending on whether a fetching request is being made.
+ * If a fetching request is in progress, the button is temporarily disabled)
  *
  * @param {Object} props - The properties passed to the component.
  * @param {boolean} props.isLoading - Indicates if the fetching request is being made.
