316e4953b59659790520c679dfeb88411a365f84
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 16:48:00 +0200

    add jsdoc to textarea component

--- client/src/components/form/textarea/textarea.jsx
+++ client/src/components/form/textarea/textarea.jsx
@@ -3,6 +3,16 @@ import { useFormContext } from "react-hook-form";
 import Label from "../label/label";
 import TextareaChars from "../textarea-chars/textarea-chars";
 
+/**
+ * Textarea component.
+ * It renders a textarea field.
+ *
+ * (It renders the Label and the TextareaChars components)
+ *
+ * (This component is rendered only in forms used to create/update tvseries, as the "note")
+ *
+ * @returns {JSX.Element} The rendered Textarea component.
+ */
 export default function Textarea() {
   const { register } = useFormContext();
 
