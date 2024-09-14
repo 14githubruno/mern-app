bb9819fbcf1d1e04efa0a3a723313bd523d10329
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 18:15:28 +0200

    add jsdoc to user-profile-button-links-container component

--- client/src/components/user-profile-button-links-container/user-profile-button-links-container.jsx
+++ client/src/components/user-profile-button-links-container/user-profile-button-links-container.jsx
@@ -2,6 +2,15 @@ import styles from "./user-profile-button-links-container.module.scss";
 import { Link } from "react-router-dom";
 import { memo } from "react";
 
+/**
+ * UserProfileButtonLinksContainer component.
+ * It renders two links and a button to be displayed in the user profile page: one link to the dashboard, one link to update user page and one button to open modal to delete user acccount.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {function} props.toggleModalToDelete - A function to toggle the modal delete visibility.
+ *
+ * @returns {JSX.Element} The rendered UserProfileButtonLinksContainer component.
+ */
 function UserProfileButtonLinksContainer({ toggleModalToDelete }) {
   return (
     <div className={styles.userProfileButtonLinkContainer}>
