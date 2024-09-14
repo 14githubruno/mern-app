2884133b0bf282131e7801d7a53b002a37acc999
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 18:45:29 +0200

    refact jsdoc of header component

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -14,7 +14,8 @@ import toast from "react-hot-toast";
 /**
  * Header component.
  * It renders the navigation bar of the web app.
- * The content of Header component changes depending on whether there is a logged in user.
+ *
+ * (The content of Header component changes depending on whether there is a logged in user)
  *
  * @param {Object} props - The properties passed to the component.
  * @param {boolean} [props.replace=false] - Replace the current history state when navigating. Defaults to false.
