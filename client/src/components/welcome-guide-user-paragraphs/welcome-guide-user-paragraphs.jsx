888637e7c5fe52d49058c965487b2e42c763d6c7
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 18:24:58 +0200

    add jsdoc to welcome-guide-user-paragraphs component

--- client/src/components/welcome-guide-user-paragraphs/welcome-guide-user-paragraphs.jsx
+++ client/src/components/welcome-guide-user-paragraphs/welcome-guide-user-paragraphs.jsx
@@ -2,6 +2,18 @@ import styles from "./welcome-guide-user-paragraphs.module.scss";
 import { Link } from "react-router-dom";
 import { memo } from "react";
 
+/**
+ * WelcomeGuideUserParagraphs component.
+ * It renders two paragraphs above the table and below the searchbar, in the dashboard page.
+ *
+ * (The first paragraph is a welcoming message to the logged in user; the second one provides a link to the page where user can create a tvseries)
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {string} props.userLoggedIn - The name of the logged in user.
+ * @param {string} props.kreateTvseriesRoute - A paragraph displaying a link to the page where user can create a tvseries (that is a future TableRow).
+ *
+ * @returns {JSX.Element} The rendered WelcomeGuideUserParagraphs component.
+ */
 function WelcomeGuideUserParagraphs({ userLoggedIn, kreateTvseriesRoute }) {
   return (
     <div className={styles.welcomeGuideUserParagraphs}>
