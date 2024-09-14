cd380a2d1322b5ea465ad4436037e9db38bcee15
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 15:41:14 +0200

    add jsdoc to footer component

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -2,6 +2,12 @@ import styles from "./footer.module.scss";
 import { PiGithubLogo } from "react-icons/pi";
 import { Link } from "react-router-dom";
 
+/**
+ * Footer component.
+ * It renders the footer of the web app with a link to the github account of the developer.
+ *
+ * @returns {JSX.Element} The rendered Footer component.
+ */
 export default function Footer() {
   return (
     <footer className={styles.footer}>
