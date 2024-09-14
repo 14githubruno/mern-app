34d98f020a9258de4ef15e688d87fcac593bc271
bruno <bruno.lusardi.96@gmail.com>
2024-09-14 17:00:51 +0200

    add jsdoc to home content wrapper component

--- client/src/components/home-content-wrapper/home-content-wrapper.jsx
+++ client/src/components/home-content-wrapper/home-content-wrapper.jsx
@@ -1,5 +1,14 @@
 import styles from "./home-content-wrapper.module.scss";
 
+/**
+ * HomeContentWrapper component.
+ * It renders a wrapper for the homepage content.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {React.ReactNode} props.children - The homepage components/children elements to be rendered within the wrapper.
+ *
+ * @returns {JSX.Element} The rendered HomeContentWrapper component.
+ */
 export default function HomeContentWrapper({ children }) {
   return <div className={styles.homeContentWrapper}>{children}</div>;
 }
