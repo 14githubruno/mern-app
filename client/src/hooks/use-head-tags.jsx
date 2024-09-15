5eb61afe064353b21e1af775e9e5bb1e00629839
bruno <bruno.lusardi.96@gmail.com>
2024-09-15 12:30:11 +0200

    fix dep array of useHeadTags hook

--- client/src/hooks/use-head-tags.jsx
+++ client/src/hooks/use-head-tags.jsx
@@ -24,7 +24,7 @@ const useHeadTags = (page, string = "") => {
         ? `${metaDescription} [${capitalize(string)}]`
         : metaDescription;
     }
-  }, []);
+  }, [page, string]);
 };
 
 export { useHeadTags };
