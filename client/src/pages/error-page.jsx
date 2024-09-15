6c40ff1d698280c6c31e731f2cc37330419e6efb
bruno <bruno.lusardi.96@gmail.com>
2024-09-15 11:03:49 +0200

    add jsdoc to error page component

--- client/src/pages/error-page.jsx
+++ client/src/pages/error-page.jsx
@@ -3,6 +3,14 @@ import ErrorPageContent from "../components/error-page-content/error-page-conten
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 
+/**
+ * ErrorPage page component.
+ * Error page to which user is redirected if tries to access a non-existing or expired resource.
+ *
+ * (Header and Footer are here imported, whereas in the other/real web app pages are both handled by App component)
+ *
+ * @returns {JSX.Element} The rendered ErrorPage page component.
+ */
 export default function ErrorPage() {
   // this below fires a useEffect
   useHeadTags("errorPage");
