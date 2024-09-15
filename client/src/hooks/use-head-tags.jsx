347a8295dc9c0cd18318c6a2f7938692ef32629f
bruno <bruno.lusardi.96@gmail.com>
2024-09-15 12:20:40 +0200

    add jsdoc to useHeadTags custom hook

--- client/src/hooks/use-head-tags.jsx
+++ client/src/hooks/use-head-tags.jsx
@@ -2,6 +2,17 @@ import { headTags } from "../data-content/head-tags-data";
 import { capitalize } from "../lib/capitalize-string";
 import { useEffect } from "react";
 
+/**
+ * useHeadTags custom hook.
+ * It sets the appropriate document title and meta description to each page.
+ *
+ * (A config object is used to retrieve data to be used in each page.
+ * When component mounts, a useEffect will set the two head tags and populate them)
+ *
+ * @param {string} page - The path url of the current page.
+ * @param {string} string - Optional string to append to the title and/or meta description.
+ *
+ */
 const useHeadTags = (page, string = "") => {
   useEffect(() => {
     if (headTags[page] && document.title !== headTags[page].title) {
