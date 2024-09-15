e31c952bb05474a212e0961b07f3116b4211b45b
bruno <bruno.lusardi.96@gmail.com>
2024-09-15 12:09:22 +0200

    add jsdoc to app root component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,19 @@ import { Toaster } from "react-hot-toast";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 
+/**
+ * App root component.
+ * It renders/wraps all the content (pages) of the web app, through the Outlet component.
+ * Also, for each page, renders the Header and Footer components.
+ *
+ * (Header and Footer of the ErrorPage are handled directly by the latter,
+ * since the ErrorPage will be used if url path hit by the user does not match
+ * any existing path)
+ *
+ * (Here also the Toaster configuration, to allow toasting messages across the app)
+ *
+ * @returns {JSX.Element} The rendered App root component.
+ */
 export default function App() {
   return (
     <>
