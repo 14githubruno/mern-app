fe7e3c77d332fab71cf1cfffb5ebd07d2a60f213
bruno <bruno.lusardi.96@gmail.com>
2024-09-15 12:33:53 +0200

    add jsdoc to use-reset-api-and-user custom hook

--- client/src/hooks/use-reset-api-and-user.jsx
+++ client/src/hooks/use-reset-api-and-user.jsx
@@ -3,6 +3,12 @@ import { clearCredentials } from "../redux/features/auth/auth-slice";
 import { resetTvseries } from "../redux/features/tvseries/tvseries-slice";
 import { apiSlice } from "../redux/api/api-slice";
 
+/**
+ * useResetApiAndUser custom hook.
+ * It returns a function that clears local storage, resets app redux store and APIs.
+ *
+ * @returns {function} - The function to reset the app state.
+ */
 const useResetApiAndUser = () => {
   const dispatch = useDispatch();
 
