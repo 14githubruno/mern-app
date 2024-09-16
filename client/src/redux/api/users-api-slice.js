9ad00378d95ea4244da3073bfe8606c982a89891
bruno <bruno.lusardi.96@gmail.com>
2024-09-16 16:50:22 +0200

    add jsdoc to users api slice

--- client/src/redux/api/users-api-slice.js
+++ client/src/redux/api/users-api-slice.js
@@ -1,8 +1,38 @@
 import { apiSlice } from "./api-slice";
 import { API_USERS } from "../constants";
 
+/**
+ * Injects user-related endpoints in the root api slice.
+ *
+ * For each endpoint generates hooks to be exported and used across the application.
+ * @example
+ * export const userApiSlice = apiSlice.injectEndpoints({
+ *    endpoints: (builder) => ({
+ *    // create register user endpoint
+ *      registerUser: builder.mutation({
+ *        query: (credentials) => ({
+ *        url: `${API_USERS}/register`,
+ *        method: "POST",
+ *        body: credentials,
+ *      }),
+ *      // other props...
+ *    }),
+ *    // other endpoints...
+ *   })
+ * })
+ *
+ * // Export rtk hook base on endpoint from slice
+ * export const { useRegisterUserMutation } = userApiSlice;
+ *
+ * @see {@link https://redux-toolkit.js.org/tutorials/rtk-query} for more info.
+ */
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    /**
+     * POST /api/users/register
+     *
+     * Endpoint to register user.
+     */
     registerUser: builder.mutation({
       query: (credentials) => ({
         url: `${API_USERS}/register`,
@@ -11,12 +41,22 @@ export const userApiSlice = apiSlice.injectEndpoints({
       }),
     }),
 
+    /**
+     * GET /api/users/verify/:token
+     *
+     * Endpoint to verify token in params.
+     */
     verifyToken: builder.query({
       query: (token) => ({
         url: `${API_USERS}/verify/${token}`,
       }),
     }),
 
+    /**
+     * PATCH /api/users/verify/:token
+     *
+     * Endpoint to verify user and confirm user registration.
+     */
     verifyUser: builder.mutation({
       query: (symbol) => ({
         url: `${API_USERS}/verify/${symbol.token}`,
@@ -25,6 +65,11 @@ export const userApiSlice = apiSlice.injectEndpoints({
       }),
     }),
 
+    /**
+     * POST /api/users/login
+     *
+     * Endpoint to log in user.
+     */
     loginUser: builder.mutation({
       query: (credentials) => ({
         url: `${API_USERS}/login`,
@@ -33,22 +78,37 @@ export const userApiSlice = apiSlice.injectEndpoints({
       }),
     }),
 
+    /**
+     * POST /api/users/forgot-password
+     *
+     * Endpoint to start password reset.
+     */
     forgotPassword: builder.mutation({
-      query: (data) => ({
+      query: (email) => ({
         url: `${API_USERS}/forgot-password`,
         method: "POST",
-        body: data,
+        body: email,
       }),
     }),
 
+    /**
+     * PATCH /api/users/verify-password-secret/:token
+     *
+     * Endpoint to verify password secret and re-verify user.
+     */
     verifyPasswordSecret: builder.mutation({
-      query: (data) => ({
-        url: `${API_USERS}/verify-password-secret/${data.token}`,
+      query: (symbol) => ({
+        url: `${API_USERS}/verify-password-secret/${symbol.token}`,
         method: "PATCH",
-        body: data,
+        body: symbol,
       }),
     }),
 
+    /**
+     * PATCH /api/users/reset-password/:token
+     *
+     * Endpoint to complete password reset.
+     */
     resetPassword: builder.mutation({
       query: (data) => ({
         url: `${API_USERS}/reset-password/${data.token}`,
@@ -57,6 +117,11 @@ export const userApiSlice = apiSlice.injectEndpoints({
       }),
     }),
 
+    /**
+     * POST /api/users/logout
+     *
+     * Endpoint to log out user.
+     */
     logoutUser: builder.mutation({
       query: () => ({
         url: `${API_USERS}/logout`,
@@ -64,6 +129,11 @@ export const userApiSlice = apiSlice.injectEndpoints({
       }),
     }),
 
+    /**
+     * GET /api/users/profile
+     *
+     * Endpoint to get user data for profile page.
+     */
     getUserProfile: builder.query({
       query: () => ({
         url: `${API_USERS}/profile`,
@@ -71,6 +141,11 @@ export const userApiSlice = apiSlice.injectEndpoints({
       providesTags: ["User"],
     }),
 
+    /**
+     * PATCH /api/users/profile/:id
+     *
+     * Endpoint to update user profile.
+     */
     updateUserProfile: builder.mutation({
       query: (userToUpdate) => ({
         url: `${API_USERS}/profile/${userToUpdate._id}`,
@@ -80,6 +155,11 @@ export const userApiSlice = apiSlice.injectEndpoints({
       invalidatesTags: ["User"],
     }),
 
+    /**
+     * PATCH /api/users/profile/verify/:token
+     *
+     * Endpoint to confirm that user wants to update data and confirm updates.
+     */
     verifyUpdateUserProfile: builder.mutation({
       query: (symbol) => ({
         url: `${API_USERS}/profile/verify/${symbol.token}`,
@@ -89,6 +169,11 @@ export const userApiSlice = apiSlice.injectEndpoints({
       invalidatesTags: ["User"],
     }),
 
+    /**
+     * DELETE /api/users/profile/:id
+     *
+     * Endpoint to delete user account.
+     */
     deleteUserProfile: builder.mutation({
       query: (userToDelete) => ({
         url: `${API_USERS}/profile/${userToDelete._id}`,
