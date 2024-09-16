436654c20f9b86bb61a367035797157313107e50
bruno <bruno.lusardi.96@gmail.com>
2024-09-16 10:12:18 +0200

    add jsdoc to constants.js

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,5 +1,22 @@
+/**
+ * @constant
+ * The base URL for API requests.
+ * @type {string}
+ */
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+/**
+ * @constant
+ * API endpoint for user-related operations.
+ * @type {string}
+ */
 const API_USERS = "/api/users";
+
+/**
+ * @constant
+ * API endpoint for tvseries-related operations.
+ * @type {string}
+ */
 const API_TVSERIES = "/api/tvseries";
 
 export { BASE_URL, API_USERS, API_TVSERIES };
