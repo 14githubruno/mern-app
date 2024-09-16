0fcd863d85cbc24a4a78b4fd7453af03fc151380
bruno <bruno.lusardi.96@gmail.com>
2024-09-16 11:34:45 +0200

    add comments in store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,12 +4,20 @@ import { apiSlice } from "./api/api-slice";
 import authReducer from "./features/auth/auth-slice";
 import tvseriesReducer from "./features/tvseries/tvseries-slice";
 
+/**
+ * Combined reducers to be used during store configuration.
+ */
 const reducer = combineReducers({
   [apiSlice.reducerPath]: apiSlice.reducer,
   auth: authReducer,
   tvseries: tvseriesReducer,
 });
 
+/**
+ * Redux store.
+ *
+ * (Provided in main.jsx, the app entry point)
+ */
 const store = configureStore({
   reducer,
   middleware: (gDM) => gDM().concat(apiSlice.middleware),
