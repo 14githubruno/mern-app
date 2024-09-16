955497ad6c871ea985b33b6da4f389684d5a27da
bruno <bruno.lusardi.96@gmail.com>
2024-09-16 11:31:32 +0200

    add comment to base query in api slice

--- client/src/redux/api/api-slice.js
+++ client/src/redux/api/api-slice.js
@@ -1,6 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
+/**
+ * Base config of RTK Query API slice.
+ */
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
   credentials: "include",
